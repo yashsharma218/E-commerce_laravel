d022860f7e62a407168cba7f780f6c6d429e1b74
Yash Sharma <sharma.yash.6565@gmail.com>
2022-02-18 03:13:57 +0530

    Login Page Done

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,10 @@
+<div class="card">
+    <div class="card-header">
+      E-commerce
+    </div>
+    <div class="card-body">
+      <blockquote class="blockquote mb-0">
+        <footer class="blockquote-footer">This is E-commerce Webpage Footer</cite></footer>
+      </blockquote>
+    </div>
+  </div>
---
+++ resources/views/master.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-commerce</title>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"
+        integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js"
+        integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2" crossorigin="anonymous">
+    </script>
+</head>
+
+<body>
+    {{ View::make('header') }}
+    @yield('content')
+    {{ View::make('footer') }}
+
+</body>
+
+<script>
+    // $(document).ready(function(){
+    //     $("button").click(function(){
+    //         alert("Sab Kuch Badhiya hai")
+    //     })
+    // })
+</script>
+
+<style>
+    .custom-login {
+        height: 450px;
+        padding-top: 100px;
+    }
+
+</style>
+
+</html>
